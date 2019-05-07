a1a11fc25f52d6b1bb950492dd11cb404f74c3a4
zhanghengxin <812718649@qq.com>
2019-05-07 23:23:31 +0800

    format

--- example/manageMaster/my-project/build/webpack.dev.conf.js
+++ example/manageMaster/my-project/build/webpack.dev.conf.js
@@ -1,14 +1,14 @@
-'use strict'
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
-const path = require('path')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
+"use strict"
+const utils = require("./utils")
+const webpack = require("webpack")
+const config = require("../config")
+const merge = require("webpack-merge")
+const path = require("path")
+const baseWebpackConfig = require("./webpack.base.conf")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin")
+const portfinder = require("portfinder")
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -22,10 +22,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
     // these devServer options should be customized in /config/index.js
     devServer: {
-        clientLogLevel: 'warning',
+        clientLogLevel: "warning",
         historyApiFallback: {
             rewrites: [
-                { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') }
+                { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, "index.html") }
             ]
         },
         hot: true,
@@ -34,9 +34,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         host: HOST || config.dev.host,
         port: PORT || config.dev.port,
         open: config.dev.autoOpenBrowser,
-        overlay: config.dev.errorOverlay
-            ? { warnings: false, errors: true }
-            : false,
+        overlay: config.dev.errorOverlay ? { warnings: false, errors: true } : false,
         publicPath: config.dev.assetsPublicPath,
         proxy: config.dev.proxyTable,
         quiet: true, // necessary for FriendlyErrorsPlugin
@@ -46,23 +44,23 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': require('../config/dev.env')
+            "process.env": require("../config/dev.env")
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
         new webpack.NoEmitOnErrorsPlugin(),
         // https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
+            filename: "index.html",
+            template: "index.html",
             inject: true
         }),
         // copy custom static assets
         new CopyWebpackPlugin([
             {
-                from: path.resolve(__dirname, '../static'),
+                from: path.resolve(__dirname, "../static"),
                 to: config.dev.assetsSubDirectory,
-                ignore: ['.*']
+                ignore: [".*"]
             }
         ])
     ]
@@ -80,14 +78,18 @@ module.exports = new Promise((resolve, reject) => {
             devWebpackConfig.devServer.port = port
 
             // Add FriendlyErrorsPlugin
-            devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-                compilationSuccessInfo: {
-                    messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`]
-                },
-                onErrors: config.dev.notifyOnErrors
-                    ? utils.createNotifierCallback()
-                    : undefined
-            }))
+            devWebpackConfig.plugins.push(
+                new FriendlyErrorsPlugin({
+                    compilationSuccessInfo: {
+                        messages: [
+                            `Your application is running here: http://${
+                                devWebpackConfig.devServer.host
+                            }:${port}`
+                        ]
+                    },
+                    onErrors: config.dev.notifyOnErrors ? utils.createNotifierCallback() : undefined
+                })
+            )
 
             resolve(devWebpackConfig)
         }
--- example/manageMaster/my-project/src/components/HelloWorld.vue
+++ example/manageMaster/my-project/src/components/HelloWorld.vue
@@ -58,10 +58,10 @@
 
 <script>
 export default {
-    name: 'HelloWorld',
-    data () {
+    name: "HelloWorld",
+    data() {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            msg: "Welcome to Your Vue.js App"
         }
     }
 }
